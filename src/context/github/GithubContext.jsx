aadbd128500ba9e70fdce0ad61774eed5639eb97
John Norris <salaryman1990@gmail.com>
2022-06-08 02:18:07 +0900

    Added single profile search and profile information. Needs Ref ractoring and then ready for launch.

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false
   }
 
@@ -34,6 +36,55 @@ dispatch({
 })
 }
 
+const getUser = async (login) => {
+  setLoading()
+
+  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
+})
+
+if(response.status === 404) {
+  window.location = '/notfound'
+} else {
+
+const data = await response.json()
+
+dispatch({
+  type: 'GET_USER',
+  payload: data,
+})
+}
+
+}
+
+const getUserRepos = async (login) => {
+  setLoading()
+
+  const params = new URLSearchParams({
+    sort: 'created',
+    per_page: 10
+  })
+
+  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
+})
+const data = await response.json()
+
+dispatch({
+  type: 'GET_REPOS',
+  payload: data,
+})
+}
+
+
+
+const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+
+
 const setLoading = () => dispatch({type:'SET_Loading'
 
 })
@@ -41,7 +92,12 @@ const setLoading = () => dispatch({type:'SET_Loading'
   return <GithubContext.Provider value={{
       users: state.users,
       loading: state.loading,
+      user: state.user,
+      repos: state.repos,
       searchUsers,
+      clearUsers,
+      getUser,
+      getUserRepos,
     }}>
     {children}
   </GithubContext.Provider>
